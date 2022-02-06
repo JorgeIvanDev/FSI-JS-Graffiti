d0141cd2e79644d41e7bdb5ec775f2ee5b632935
jorge <ivanjorge88@gmail.com>
2022-02-06 17:23:28 -0600

    added changes

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
+let header = document.querySelector('#page-header')
 // Select some elements...
+header.style.textAlign = "right"
+
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.borderStyle = 'solid'
